9557c261904ddbf4eb3fd036e88cb62638b9e4c2
Versun <me+github7604@versun.org>
2025-06-25 16:49:30 +0800

    添加cache debug日志

--- core/cache.py
+++ core/cache.py
@@ -20,6 +20,7 @@ def make_response(atom_feed, filename, formate="xml"):
     return response
 
 def cache_rss(feed_slug:str, feed_type="t", formate="xml"):
+    logging.debug(f"Start cache_rss for {feed_slug} with type {feed_type} and format {formate}")
     # 生成唯一的缓存键
     cache_key = f'cache_rss_{feed_slug}_{feed_type}_{formate}'
 
@@ -31,10 +32,11 @@ def cache_rss(feed_slug:str, feed_type="t", formate="xml"):
     response = make_response(atom_feed, feed_slug, formate)
     # 缓存
     cache.set(cache_key, response, None)
-
+    logging.debug(f"Cached successfully with key {cache_key}")
     return response
 
 def cache_category(category:str, feed_type="t", formate="xml"):
+    logging.debug(f"Start cache_category for {category} with type {feed_type} and format {formate}")
     # 生成唯一的缓存键
     cache_key = f'cache_category_{category}_{feed_type}_{formate}'
 
@@ -47,6 +49,6 @@ def cache_category(category:str, feed_type="t", formate="xml"):
     response = make_response(atom_feed, category, formate)
     # 缓存
     cache.set(cache_key, response, None)
-
+    logging.debug(f"Cached successfully with key {cache_key}")
     return response
 
