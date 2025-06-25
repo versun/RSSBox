0b5474cb9a212934186fdf4c85a713caf65971bd
Versun <me+github7604@versun.org>
2025-06-25 18:34:52 +0800

    设置cache后端为redis

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,13 @@ DATABASES = {
         "NAME": DATA_FOLDER / "db.sqlite3",
     }
 }
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://localhost:6379/"),
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
