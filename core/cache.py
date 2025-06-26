c6e962841b114bd86c58f175b69a6d3b29839efd
Versun <me+github7604@versun.org>
2025-06-26 09:11:36 +0800

    优化缓存代码

--- core/cache.py
+++ core/cache.py
@@ -1,6 +1,5 @@
 import logging
 from django.core.cache import cache
-from django.http import HttpResponse, StreamingHttpResponse, JsonResponse
 from utils.feed_action import merge_feeds_into_one_atom, generate_atom_feed
 
 from .models import Feed
@@ -13,9 +12,8 @@ def cache_rss(feed_slug:str, feed_type="t", formate="xml"):
     feed = Feed.objects.get(slug=feed_slug)
     atom_feed = generate_atom_feed(feed, feed_type)
     if not atom_feed:
-        return HttpResponse(status=500, content="Feed not found, Maybe it's still in progress")
+        return None
     
-    # response = make_response(atom_feed, feed_slug, formate)
     # 缓存
     cache.set(cache_key, atom_feed, None)
     logging.debug(f"Cached successfully with key {cache_key}")
@@ -30,9 +28,8 @@ def cache_category(category:str, feed_type="t", formate="xml"):
     atom_feed = merge_feeds_into_one_atom(category, feeds, feed_type)
 
     if not atom_feed:
-        return HttpResponse(status=500, content="Feed not found, Maybe it's still in progress")
+        return None
     
-    # response = make_response(atom_feed, category, formate)
     # 缓存
     cache.set(cache_key, atom_feed, None)
     logging.debug(f"Cached successfully with key {cache_key}")
