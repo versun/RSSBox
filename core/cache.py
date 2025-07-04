35e07933190ab89e7bff09dbde6f5974de29a397
Versun <me+github7604@versun.org>
2025-07-04 09:49:18 +0800

    设置缓存过期日期

--- core/cache.py
+++ core/cache.py
@@ -18,7 +18,7 @@ def cache_rss(feed_slug: str, feed_type="t", format="xml"):
         return None
 
     # 缓存
-    cache.set(cache_key, atom_feed, None)
+    cache.set(cache_key, atom_feed, feed.update_frequency or 86400) # default to 1 day
     logging.debug(f"Cached successfully with key {cache_key}")
     return atom_feed
 
@@ -31,12 +31,13 @@ def cache_category(category: str, feed_type="t", format="xml"):
     cache_key = f"cache_category_{category}_{feed_type}_{format}"
 
     feeds = Feed.objects.filter(category=category)
+    max_frequency_feed = feeds.order_by('-update_frequency').first()
     atom_feed = merge_feeds_into_one_atom(category, feeds, feed_type)
 
     if not atom_feed:
         return None
 
     # 缓存
-    cache.set(cache_key, atom_feed, None)
+    cache.set(cache_key, atom_feed, max_frequency_feed.update_frequency or 86400)
     logging.debug(f"Cached successfully with key {cache_key}")
     return atom_feed
